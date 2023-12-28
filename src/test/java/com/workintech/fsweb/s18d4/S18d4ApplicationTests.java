8395c5797d48f328dac3845c4ff11743ae8275be
Kursad Cengiz Yildirim <k.cengizyildirim@gmail.com>
2023-12-28 03:52:10 +0300

    project init

---
+++ src/test/java/com/workintech/fsweb/s18d4/S18d4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.fsweb.s18d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S18d4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
